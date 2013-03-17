cade8dcb7c86f0bd7ae2541a11795a76601bb10c
blacklaw <2577927146@qq.com>
2013-03-17 18:25:00 +0800

    Create win8.js
    win8.js for http://townke.com

---
+++ win8.js
@@ -0,0 +1,211 @@
+  
+p{
+	margin:3px;
+}
+img{
+	border-style:none;
+}
+	#wrap div{
+	margin:5px;	
+	}
+	div.sty1,div.sty2{
+
+
+	background:#039;
+	color:#CF3;
+	height:160px;
+	width:330px;
+	cursor:'url(./mouse.ico)';
+			float:left;
+	}
+	div.sty1:hover,div.sty2:hover{
+		background:#900!important;
+
+	}
+	div.sty2{
+
+	height:160px;
+	width:160px;
+	}
+body{
+	background:#339933;
+	margin:auto;
+
+/*	border:1px solid red;**/
+	
+
+	}
+	#wrap{	
+
+	width:850px;
+	/*	background:#000;*/
+	margin:auto;
+	margin-top:100px;
+
+    overflow:hidden;
+
+	}
+	img.longblock,#leave_img{
+	margin-left:15px;
+	margin-top:15px;
+		
+	}
+	img.longblock_right{
+		float:right;
+	margin-right:15px;
+	margin-top:15px;
+		
+	}
+#down{
+background: #e51400 ;	
+}
+#rss{
+	background:#1ba1e2;
+	
+}
+
+#email{
+	background: #d54d34 ;	
+}
+#count{
+	display:none;
+	
+}
+#menu{
+	
+	height:50px;
+	background:#333	;
+	position:absolute;
+	width:100%;
+	bottom:0px;
+	
+}
+
+span.menuButton{
+	width:80px;
+	height:50px;
+	float:left;	
+}
+span.menuButton:hover{
+	background:#06F;	
+}
+#menu_block_right{
+	float:right;
+}
+#menu_block_left{
+	float:left;
+}
+#menu img{
+	margin-top:10px;
+	margin-left:25px;
+}
+#user_img:hover{
+	background:#0CF;
+	}
+
+#user{
+	margin-right:50px;
+	margin-top:50px;
+	float:right;
+}
+#user span{
+	float:left;
+
+}
+#weather{
+
+	float:left;
+	/*background:#009;*/
+	height:150px;
+	color:#FFF;
+	font-family:Calibri;
+}
+#w_txt_1{
+	font-size:30px;
+	font-weight:60;
+}
+#w_txt_2{
+	
+}
+#weather_img{
+	margin-left:10px;
+	margin-right:10px;
+	margin-top:10px;
+	width: 50px;
+	float:left;
+}
+#leave_note{
+	width:170px;
+	height:150px;
+	/*background:#0C9;*/
+		float:left;
+		color:#FFF;
+		font-family:Calibri;
+			font-size:14px;
+		overflow:hidden;
+}
+#leave_img{
+	float:left;
+}
+
+#leave_IP{
+	font-style:oblique;	
+}
+#leave_TIME{
+
+}
+#leave_MESSAGE{
+	
+}
+#leaveBox{
+	background:#6C9;
+	position:absolute;
+	color:#FFF;
+	font-family:Calibri;
+	left:10px;
+	top:10px;
+	width:200px;
+	height:300px;
+}
+#leaveTitle{
+	overflow:hidden;
+	background:#e671b8;
+	cursor:move;
+}
+#leaveTitle .titleL{
+	float:left;	
+}
+#leaveTitle .titleX{
+	float:right;
+	cursor:default;
+	/*background:#6C9;*/
+}
+#leaveTitle .titleX:hover{
+	background:#0C9;	
+}
+#formMessage{
+	resize:none;
+	background:	#6C9;
+	width:97%;
+	height:130px;
+	color:#FFF;
+	border-color:#FFF;
+	border-style:dashed;
+}
+.divButton{
+	width:70px;
+	height:40px;
+	margin-bottom:20px;
+	background:#09C;
+	text-align:center;
+	line-height:30px;
+	margin:auto;
+}
+.divButton:hover{
+	background:#F39;
+	cursor:pointer;
+}
+#leaveBody{
+	/*background:#039;*/
+	height:80%;
+}
